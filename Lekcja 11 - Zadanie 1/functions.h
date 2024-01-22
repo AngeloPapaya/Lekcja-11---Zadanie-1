d7475d1abaff73561415b3723ac1bf81127d6a51
AngeloPapaya <147622287+AngeloPapaya@users.noreply.github.com>
2024-01-22 19:02:58 +0100

    Update functions.h

--- Lekcja 11 - Zadanie 1/functions.h
+++ Lekcja 11 - Zadanie 1/functions.h
@@ -1,22 +1,4 @@
 #pragma once
+
 int GetNumber();
 char GetSign();
-
-template <typename Type1, typename Type2>
-void Compare (Type1 a, Type2 b)
-{
-    if (a > b) std::cout << a << " jest wieksze."<< "\n";
-    else if (a == b) std::cout << "a i b sa rowne."<< "\n";
-    else std::cout << b << " jest wieksze."<< "\n";
-}
-
-template<>
-void Compare (char a, char b)
-{
-    if (a > b) std::cout << a << " ma wieksza wartosc w tablicy ASCII." << "\n";
-    else if (a == b) std::cout << "a i b sa rowne."<< "\n";
-    else std::cout << b << " ma wieksza wartosc w tablicy ASCII."<< "\n";
-}
-
-
-
