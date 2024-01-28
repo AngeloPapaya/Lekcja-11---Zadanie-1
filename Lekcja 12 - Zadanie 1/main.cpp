624068bb371e2cf8143fff38c98057782cbb1345
AngeloPapaya <147622287+AngeloPapaya@users.noreply.github.com>
2024-01-28 21:00:24 +0100

    Update main.cpp

--- Lekcja 12 - Zadanie 1/main.cpp
+++ Lekcja 12 - Zadanie 1/main.cpp
@@ -11,31 +11,32 @@ int main()
     const std::string b = "Tekst2";
     std::string &a1 = a;
     const std::string &b1 = b;
-    
-    a = b;
-    a = a1;
-    a = b1;
-    
-    b = a; //b jest niezmienne i nie mozna przypisac do b innej zmiennej
-    b = a1;
-    b = b1;
-    
-    a1 = a;
-    a1 = b;
-    a1 = b1;
-    
-    
-    b1 = a;
-    b1 = a1;
-    b1 = b;
 
-return 0;
+    a = b; //a = Tekst2
+    a = a1; // a = a1 = Tekst2
+    a = b1;//a = b1 = b  = Tekst23
 
+//    b = a; //b jest niezmienne i nie mozna przypisac do b innej zmiennej
+//    b = a1;
+//    b = b1;
+
+    a1 = a;//referencja do a
+    a1 = b;//referencja do b
+    a1 = b1;//referencja do b
+
+
+//    b1 = a;
+//    b1 = a1;
+//    b1 = b;
+
+
+    return 0;
 
 
 
 
 }
+
 /*
 w Zadaniu 1 powinieneś zainicjalizować na początku 4 zmienne
 A. string
