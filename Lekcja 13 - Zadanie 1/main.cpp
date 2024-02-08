6b3943888f37d41edd0f3e68a3040209bc684a95
AngeloPapaya <147622287+AngeloPapaya@users.noreply.github.com>
2024-02-08 19:20:58 +0100

    Add files via upload

--- Lekcja 13 - Zadanie 1/main.cpp
+++ Lekcja 13 - Zadanie 1/main.cpp
@@ -8,15 +8,16 @@ std::string* CreateNewString()
     std::string* test = new std::string;
     *test = InputFromUser;
     return test;
-    delete test;
 }
 
 
 int main()
 {
+
    std::string *test1 = CreateNewString();
    std::string *test2 = CreateNewString();
    std::string *test3 = CreateNewString();
-
    std::cout << *test1 << " " << *test2 << " " << *test3;
+
+   delete test1, test2, test3;
 }
